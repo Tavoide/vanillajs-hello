50eb3eb178b269fdb312fc3844ccde9473148078
Tavoide <gustavobenitez116@gmail.com>
2024-10-17 03:36:52 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excusa").innerHTML = excusas();
+  document.querySelector("#text").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = excusas();
+  });
 };
+
+function excusas() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let sugeto = Math.floor(Math.random() * who.length);
+  let verbo = Math.floor(Math.random() * action.length);
+  let ubicacion = Math.floor(Math.random() * what.length);
+  let dia = Math.floor(Math.random() * when.length);
+
+  return (
+    who[sugeto] + " " + action[verbo] + " " + what[ubicacion] + " " + when[dia]
+  );
+}
